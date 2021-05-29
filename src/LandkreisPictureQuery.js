626462d60b73bfc871d41c400cacf8f1cca2bd98
maste150hhu <mastremolten2@gmail.com>
2021-05-29 23:59:58 +0200

    fix firefox

--- src/LandkreisPictureQuery.js
+++ src/LandkreisPictureQuery.js
@@ -1,16 +1,16 @@
 class LandkreisPictureQuery {
 
-    /*private String*/ #landkreis;
-    /*private String*/ static #apiRequestURL = "https://pixabay.com/api/";
-    /*private String (devToken)*/ static #apiRequestToken = "18455198-2cc93b5bfbf3e988d656de091";
+    /*String*/ landkreis;
+    /*String*/ static apiRequestURL = "https://pixabay.com/api/";
+    /*String (devToken)*/ static apiRequestToken = "18455198-2cc93b5bfbf3e988d656de091";
 
     constructor(landkreis) {
-        this.#landkreis = landkreis;
+        this.landkreis = landkreis;
     }
 
     static async requestPictureFromAPI(query) {
         let result = null;
-        await fetch([this.#apiRequestURL, ["key=" + this.#apiRequestToken, "q=" + query.#landkreis].join("&")].join("?"))
+        await fetch([this.apiRequestURL, ["key=" + this.apiRequestToken, "q=" + query.landkreis].join("&")].join("?"))
             .then(res => res.json())
             .then(json => result = (json.totalHits > 0) ? json.hits : null);
 
