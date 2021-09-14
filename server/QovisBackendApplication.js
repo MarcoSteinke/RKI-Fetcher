c5bbd67ff48d018f65e177ad15e401389a60d390
maste150hhu <mastremolten2@gmail.com>
2021-09-14 13:39:59 +0200

    add automatic updates for rki information and api routes for hotspots and safest

--- server/QovisBackendApplication.js
+++ server/QovisBackendApplication.js
@@ -2,16 +2,38 @@ const express = require('express');
 const app = express()
 const port = 3000
 
+const SIX_HOURS = 6 * 3600 * 1000;
+
 const BackendRKIFetcher = require("./fetcher/BackendRKIFetcher.js");
 
 const SessionManager = require("./web/SessionManager.js");
 
+let i = 1;
+let content = [];
 
-
-app.get('/', SessionManager.asyncMiddleware(async (req, res, next) => {
+async function prepareData() {
+    BackendRKIFetcher.storedData = [];
     await BackendRKIFetcher.getAllLandkreiseAsObjects();
     console.log(BackendRKIFetcher.storedData);
+}
+
+prepareData();
+
+setInterval(prepareData, SIX_HOURS);
+
+app.get('/', SessionManager.asyncMiddleware(async (req, res, next) => {
+    console.log(BackendRKIFetcher.storedData);
     res.send('hi');
+}));
+
+app.get('/hotspots', SessionManager.asyncMiddleware(async (req, res, next) => {
+    console.log(BackendRKIFetcher.findHotspots());
+    res.send(JSON.stringify(BackendRKIFetcher.findHotspots()));
+}));
+
+app.get('/safest', SessionManager.asyncMiddleware(async (req, res, next) => {
+    console.log(BackendRKIFetcher.findSafestAreas());
+    res.send(JSON.stringify(BackendRKIFetcher.findSafestAreas()));
 }));
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
